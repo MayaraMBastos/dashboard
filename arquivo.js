f7c8d0430d5c497b1e22e53676a960713d30e1c1
Mayara Martinello Bastos <maybastos2021@gmail.com>
2023-02-05 20:04:40 -0300

    js main atualizado

--- arquivo.js
+++ arquivo.js
@@ -3,9 +3,11 @@ var toggleStatus = 1
 function menuResponsivo(){
   if (toggleStatus == 1) {
     document.getElementById("navegador").style.width = "300px";
+    document.querySelector("div.main").style.left = "300px";
     toggleStatus = 0;
   } else if (toggleStatus == 0) {
     document.getElementById("navegador").style.width = "80px";
+    document.querySelector("div.main").style.left = "80px";
     toggleStatus = 1;
   }
 }
