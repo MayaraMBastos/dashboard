09c54ebc7d80246a6c11a44b51f90118084323b3
Mayara Martinello Bastos <maybastos2021@gmail.com>
2023-02-05 20:43:30 -0300

    js width da classe main

--- arquivo.js
+++ arquivo.js
@@ -4,10 +4,12 @@ function menuResponsivo(){
   if (toggleStatus == 1) {
     document.getElementById("navegador").style.width = "300px";
     document.querySelector("div.main").style.left = "300px";
+    document.querySelector("div.main").style.width = "calc(100% - 300px)";
     toggleStatus = 0;
   } else if (toggleStatus == 0) {
     document.getElementById("navegador").style.width = "80px";
     document.querySelector("div.main").style.left = "80px";
+    document.querySelector("div.main").style.width = "calc(100% - 80px)";
     toggleStatus = 1;
   }
 }
