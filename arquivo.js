192dd4c9844372215d5e0d724d5b4d89d414c22c
Mayara Martinello Bastos <maybastos2021@gmail.com>
2023-02-10 21:33:25 -0300

    Responsividade 991

--- arquivo.js
+++ arquivo.js
@@ -2,14 +2,15 @@
 var estadoMenu = 0
 function menuResponsivo(){
   if (estadoMenu == 1) {
-    document.getElementById("navegador").style.width = "300px";
+    document.querySelector("div.navegador").style.width = "300px";
     document.querySelector("div.main").style.left = "300px";
     document.querySelector("div.main").style.width = "calc(100% - 300px)";
     estadoMenu = 0;
   } else if (estadoMenu == 0) {
-    document.getElementById("navegador").style.width = "80px";
-    document.getElementById("navegador").classList.add("ativo")
+    document.querySelector("div.navegador").style.width = "80px";
+    document.querySelector("div.navegador").classList.add("ativo")
     document.querySelector("div.main").style.left = "80px";
+    document.querySelector("div.main").classList.add("ativo")
     document.querySelector("div.main").style.width = "calc(100% - 80px)";
     estadoMenu = 1;
   }
