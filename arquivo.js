e0a92484979ce1acfd7b4849476fecb6c5ee0a7c
Mayara Martinello Bastos <maybastos2021@gmail.com>
2023-02-12 12:31:11 -0300

    correção responsividade

--- arquivo.js
+++ arquivo.js
@@ -2,15 +2,17 @@
 var estadoMenu = 0
 function menuResponsivo(){
   if (estadoMenu == 1) {
+    document.querySelector("div.navegador").classList.remove("ativo")
+    document.querySelector("div.main").classList.remove("ativo")
     document.querySelector("div.navegador").style.width = "300px";
     document.querySelector("div.main").style.left = "300px";
     document.querySelector("div.main").style.width = "calc(100% - 300px)";
     estadoMenu = 0;
   } else if (estadoMenu == 0) {
-    document.querySelector("div.navegador").style.width = "80px";
     document.querySelector("div.navegador").classList.add("ativo")
-    document.querySelector("div.main").style.left = "80px";
     document.querySelector("div.main").classList.add("ativo")
+    document.querySelector("div.navegador").style.width = "80px";
+    document.querySelector("div.main").style.left = "80px";
     document.querySelector("div.main").style.width = "calc(100% - 80px)";
     estadoMenu = 1;
   }
